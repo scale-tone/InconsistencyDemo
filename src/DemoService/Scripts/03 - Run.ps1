2c5fad72ed35d654fe2e5a1570fa2c72b4d14782
Ståle Skaland <stale.skaland@if.no>
2019-09-24 10:24:18 +0200

    Updated to new project file structure.

--- src/DemoService/Scripts/03 - Run.ps1
+++ src/DemoService/Scripts/03 - Run.ps1
@@ -3,6 +3,6 @@ param()
 
 Write-Verbose "Starting the applications.."
 
-Start-Process cmd -ArgumentList "/c $PSScriptRoot\..\WcfService\bin\Debug\WcfService.exe"
-Start-Process cmd -ArgumentList "/c $PSScriptRoot\..\DemoService\bin\Debug\NServiceBus.Host.exe"  -Verb runas
-Start-Process cmd -ArgumentList "/c $PSScriptRoot\..\MessageCreator\bin\Debug\MessageCreator.exe" -Verb runas
+Start-Process cmd -ArgumentList "/c $PSScriptRoot\..\WcfService\bin\Debug\net471\WcfService.exe"
+Start-Process cmd -ArgumentList "/c $PSScriptRoot\..\DemoService\bin\Debug\net471\NServiceBus.Host.exe"  -Verb runas
+Start-Process cmd -ArgumentList "/c $PSScriptRoot\..\MessageCreator\bin\Debug\net471\MessageCreator.exe" -Verb runas
